19100b4ad2cfca836a9f3d2fa76280118f6e42ee
chaebeen <tnwl05200@gmail.com>
2024-09-09 16:54:09 +0900

    modify

--- src/main.js
+++ src/main.js
@@ -1,44 +1,45 @@
 //Home 섹션 아래로 스크롤시 투명하게 처리함
 
-const home = document.querySelector('.home__container');
+const home = document.querySelector(".home__container");
 
-
-// const home = document.querySelector('.home__container');
-// const homeHeight = home.offsetHeight;
-// document.addEventListener('scroll' , () => {
-//     home.style.opacity = 1 - window.scrollY / (homeHeight / 2.5); 
+// const home = document.querySelector(".home__container");
+// document.addEventListener("scroll", () => {
+//   home.style.opacity = 1 - window.scrollY / (homeHeight / 2.5);
 // });
 
 // Arrow up 버튼을 아래로 스크롤시 투명하게 처리함
 
-const arrowUp = document.querySelector('.arrow-up');
-document.addEventListener('scroll' , () => {
-    if(window.scrollY > homeHeight / 2) {
-        arrowUp.style.opacity = 1;
-    } else {
-        arrowUp.style.opacity = 0;
-    }
-})
+// const homeHeight = home.offsetHeight;
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
 
-    document.addEventListener('DOMContentLoaded', function () {
-    let fadeInElement = document.querySelector('.footer_fade');
-    let lastScrollTop = 0;
+// 프로젝트 스크롤
+document.addEventListener("DOMContentLoaded", function () {
+  const targets = document.querySelectorAll(".project_container");
 
-    function checkScroll() {
-        let elementPosition = fadeInElement.getBoundingClientRect().top;
-        let windowHeight = window.innerHeight;
-        let currentScrollTop = window.scrollY || document.documentElement.scrollTop;
-
-        if (currentScrollTop > lastScrollTop && elementPosition < windowHeight) {
-        // 스크롤 다운
-        fadeInElement.classList.add('fadein');
-        } else if (currentScrollTop < lastScrollTop) {
-        // 스크롤 업
-        fadeInElement.classList.remove('fadein');
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
         }
-
-        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
+      });
+    },
+    {
+      threshold: 0.5, // 요소의 10%가 보일 때 trigger
     }
+  );
+  // 모든 content_tab 요소를 관찰하도록 설정
+  targets.forEach((target) => observer.observe(target));
 
-    window.addEventListener('scroll', checkScroll);
-    });
+  const targetFooter = document.querySelector(".footer_fade");
+  observer.observe(targetFooter);
+});
