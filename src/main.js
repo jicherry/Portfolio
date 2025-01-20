c4a89c0d748a917fffa0886268478ac492ba5583
chaebeen <tnwl05200@gmail.com>
2025-01-21 03:39:03 +0900

    pt수정

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-//Home 섹션 아래로 스크롤시 투명하게 처리
 window.addEventListener("scroll", function () {
   const header = document.querySelector(".header");
-  if (window.scrollY > 610) {
+  const scrollThreshold = window.innerHeight * 0.96;
+
+  if (window.scrollY > scrollThreshold) {
     header.classList.add("scrolled");
   } else {
     header.classList.remove("scrolled");
@@ -19,6 +20,45 @@ document.addEventListener("scroll", () => {
   }
 });
 
+//헤더부분
+document.addEventListener("DOMContentLoaded", () => {
+  const navItems = document.querySelectorAll(".navigation a");
+
+  navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      navItems.forEach((nav) => nav.classList.remove("selected"));
+
+      item.classList.add("selected");
+    });
+  });
+  const typingTexts = document.querySelectorAll(".typing-text");
+
+  typingTexts.forEach((typingText) => {
+    typingText.addEventListener("animationend", () => {
+      typingText.classList.add("animation-complete");
+    });
+  });
+});
+
+//푸터
+document.addEventListener("scroll", () => {
+  const footerTexts = document.querySelectorAll(".f_text");
+  const footerContainer = document.querySelector(".footer_container");
+  const screenHeight = window.innerHeight;
+  const footerTop = footerContainer.getBoundingClientRect().top;
+
+  // 화면의 80% 지점에 도달했을 때 애니메이션 시작
+  if (footerTop < screenHeight * 0.8) {
+    footerTexts.forEach((text, index) => {
+      text.style.animation = "none";
+      void text.offsetWidth;
+      text.style.animation = `dropText 1.2s ease-in-out ${
+        index * 0.2
+      }s forwards`;
+    });
+  }
+});
+
 //arrow up
 window.addEventListener("scroll", function () {
   const arrowUp = document.querySelector(".arrow-up");
@@ -29,30 +69,4 @@ window.addEventListener("scroll", function () {
   } else {
     arrowUp.classList.remove("show");
   }
-});
-
-// 프로젝트 스크롤
-document.addEventListener("DOMContentLoaded", function () {
-  const targets = document.querySelectorAll(".project_container");
-
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-        } else {
-          entry.target.classList.remove("show");
-        }
-      });
-    },
-    {
-      threshold: 0.5, //
-    }
-  );
-
-  // 모든 content_tab 요소
-  targets.forEach((target) => observer.observe(target));
-
-  const targetFooter = document.querySelector(".footer_fade");
-  observer.observe(targetFooter);
 });
